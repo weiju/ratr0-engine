b53ad688349dcb57651aed858d227b75e10ffc18
weiju <weiju.wu@gmail.com>
2023-03-27 12:11:42 -0700

    added Amiga specific directory

--- src/amiga/display.c
+++ src/amiga/display.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <ratr0/debug_utils.h>
-#include <ratr0/display.h>
-
-#ifdef AMIGA
 #include <ratr0/amiga/display.h>
-#endif /* AMIGA */
 
-#define PRINT_DEBUG(...) PRINT_DEBUG_TAG("\033[32mDISPLAY\033[0m", __VA_ARGS__)
+#define PRINT_DEBUG(...) PRINT_DEBUG_TAG("\033[32mDISPLAY[AMIGA]\033[0m", __VA_ARGS__)
 
-extern void ratr0_display_startup()
+extern void ratr0_amiga_display_startup()
 {
     PRINT_DEBUG("Start up...");
     PRINT_DEBUG("Startup finished.");
 }
 
-extern void ratr0_display_shutdown()
+extern void ratr0_amiga_display_shutdown()
 {
     PRINT_DEBUG("Shutting down...");
     PRINT_DEBUG("Shutdown finished.");
---
+++ src/include/ratr0/amiga/display.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef __RATR0_AMIGA_DISPLAY_H__
+#define __RATR0_AMIGA_DISPLAY_H__
+
+/* Amiga Display subsystem */
+
+/**
+ * Start up the display subsystem.
+ */
+extern void ratr0_amiga_display_startup();
+
+/**
+ * Shut down the display subsystem.
+ */
+extern void ratr0_amiga_display_shutdown();
+
+
+#endif /* __RATR0_AMIGA_DISPLAY_H__ */
