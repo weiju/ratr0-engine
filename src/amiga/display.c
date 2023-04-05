ae676588d4483a392d0cfae668d826f394ea851e
weiju <weiju.wu@gmail.com>
2023-04-05 09:23:09 -0700

    Added display cleanup

--- src/amiga/display.c
+++ src/amiga/display.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+#include <hardware/custom.h>
+#include <graphics/gfxbase.h>
+#include <clib/intuition_protos.h>
 #include <clib/graphics_protos.h>
 
 #include <ratr0/debug_utils.h>
 #include <ratr0/amiga/display.h>
 
 #define PRINT_DEBUG(...) PRINT_DEBUG_TAG("\033[32mDISPLAY[AMIGA]\033[0m", __VA_ARGS__)
+
+/*
+ * We assume these to exist.
+ */
+extern struct GfxBase *GfxBase;
+extern struct Custom custom;
 
 void ratr0_amiga_display_startup(void)
 {
@@ -20,5 +29,11 @@ void ratr0_amiga_display_startup(void)
 void ratr0_amiga_display_shutdown(void)
 {
     PRINT_DEBUG("Shutting down...");
+    // Restore the Workbench display by restoring the original copper list
+    LoadView(((struct GfxBase *) GfxBase)->ActiView);
+    WaitTOF();
+    WaitTOF();
+    custom.cop1lc = (ULONG) ((struct GfxBase *) GfxBase)->copinit;
+    RethinkDisplay();
     PRINT_DEBUG("Shutdown finished.");
 }
