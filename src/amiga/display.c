7264b99831045d0198fb5f6a6b50b512cfbf5bee
weiju <weiju.wu@gmail.com>
2023-04-05 15:15:07 -0700

    Added a copper list initialization

--- src/amiga/display.c
+++ src/amiga/display.c
@@ -6,6 +6,7 @@
 #include <clib/graphics_protos.h>
 
 #include <ratr0/debug_utils.h>
+#include <ratr0/amiga/hw_registers.h>
 #include <ratr0/amiga/display.h>
 #include <ratr0/memory.h>
 
@@ -43,7 +44,70 @@ extern struct Custom custom;
 
 static struct Ratr0AmigaDisplayInfo display_info;
 static Ratr0MemHandle h_copper_list;
+static UINT16 *copper_list;
 
+// Data fetch
+#define DDFSTRT_VALUE      0x0038
+#define DDFSTOP_VALUE      0x00d0
+
+// Display window
+#define DIWSTRT_VALUE      0x2c81
+#define DIWSTOP_VALUE_PAL  0x2cc1
+#define DIWSTOP_VALUE_NTSC 0xf4c1
+
+
+/* This is a bit of a trick: I pre-allocate dummy sprite data */
+UWORD __chip NULL_SPRITE_DATA[] = {
+    0x0000, 0x0000,
+    0x0000, 0x0000
+};
+
+UINT16 _cop_move(UINT16 addr, UINT16 value, UINT16 index)
+{
+    copper_list[index++] = addr;
+    copper_list[index++] = value;
+    return index;
+}
+UINT16 _cop_wait_end(UINT16 index)
+{
+    copper_list[index++] = 0xffff;
+    copper_list[index++] = 0xfffe;
+    return index;
+}
+
+void build_copper_list()
+{
+    int cl_index = 0;
+    cl_index = _cop_move(FMODE, 0, cl_index);  // 4 bytes
+
+    // Initialize the sprites with the NULL address (8x8 = 64 bytes)
+    UINT16 spr_ptr = SPR0PTH;
+    for (int i = 0; i < 8; i++) {
+        cl_index = _cop_move(spr_ptr, (((ULONG) NULL_SPRITE_DATA) >> 16) & 0xffff, cl_index);
+        cl_index = _cop_move(spr_ptr + 2, ((ULONG) NULL_SPRITE_DATA) & 0xffff, cl_index);
+        spr_ptr += 4;
+    }
+
+    // set up the display and DMA windows (16 bytes)
+    cl_index = _cop_move(DDFSTRT, DDFSTRT_VALUE, cl_index);
+    cl_index = _cop_move(DDFSTOP, DDFSTOP_VALUE, cl_index);
+    cl_index = _cop_move(DIWSTRT, DIWSTRT_VALUE, cl_index);
+    if (display_info.is_pal) {
+        cl_index = _cop_move(DIWSTOP, DIWSTOP_VALUE_PAL, cl_index);
+    } else {
+        cl_index = _cop_move(DIWSTOP, DIWSTOP_VALUE_NTSC, cl_index);
+    }
+
+    // TODO: set up the bitmap for the display
+    // 1. BPLCONx
+    // 2. BPLxMOD
+    // 3. COLORxx
+    // 4. BPLxPTH/BPLxPTL
+    // TODO: save the indexes for quick access so we can change them in the game
+
+    // End the copper list (4 bytes)
+    cl_index = _cop_wait_end(cl_index);
+}
 void ratr0_amiga_display_startup(struct Ratr0AmigaDisplayInfo *init_data)
 {
     PRINT_DEBUG("Start up...");
@@ -59,8 +123,11 @@ void ratr0_amiga_display_startup(struct Ratr0AmigaDisplayInfo *init_data)
     display_info.depth = init_data->depth;
     display_info.is_pal = (((struct GfxBase *) GfxBase)->DisplayFlags & PAL) == PAL;
 
-    int num_entries = 6;
-    h_copper_list = Ratr0MemoryService.allocate_block(RATR0_MEM_CHIP, num_entries * sizeof(UINT16));
+    int cl_num_bytes = 96;
+    h_copper_list = Ratr0MemoryService.allocate_block(RATR0_MEM_CHIP, cl_num_bytes);
+    copper_list = Ratr0MemoryService.block_address(h_copper_list);
+    build_copper_list();
+    custom.cop1lc = (ULONG) copper_list;
     PRINT_DEBUG("Startup finished.");
 }
 
