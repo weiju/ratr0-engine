1a6df3110f2690938b6fa12e5eca4d7e899ee753
weiju <weiju.wu@gmail.com>
2023-04-07 21:11:04 -0700

    integrate tilesheet reader with engine

--- src/include/ratr0/resources.h
+++ src/include/ratr0/resources.h
@@ -1,15 +1,48 @@
 #pragma once
 #ifndef __RATR0_RESOURCES_H__
 #define __RATR0_RESOURCES_H__
+#include <ratr0/data_types.h>
+#include <ratr0/engine.h>
+#include <ratr0/memory.h>
 
 /* Resources subsystem */
+// information about a tile sheet
+#define FILE_ID_LEN (8)
+
+// information about a tile sheet
+// File format version 2
+// changes to version 1:
+//   1. dropped the reserved2 word after palette_size
+//   2. changed size of checksum from ULONG to UWORD
+struct Ratr0TileSheetHeader {
+    UINT8 id[FILE_ID_LEN];
+    UINT8 version, flags;
+    UINT8 reserved1, bmdepth;
+    UINT16 width, height;
+    UINT16 tile_width, tile_height;
+    UINT16 num_tiles_h, num_tiles_v;
+    UINT16 palette_size;
+    UINT32 imgdata_size;
+    UINT16 checksum;
+};
+
+#define MAX_PALETTE_SIZE (32)
+struct Ratr0TileSheet {
+    struct Ratr0TileSheetHeader header;
+    UINT16 palette[MAX_PALETTE_SIZE];
+    /* Use memory handle */
+    Ratr0MemHandle h_imgdata;
+};
+
 struct Ratr0ResourceSystem {
+    UINT32 (*read_tilesheet)(const char *filename, struct Ratr0TileSheet *sheet);
+    void (*free_tilesheet_data)(struct Ratr0TileSheet *sheet);
     void (*shutdown)(void);
 };
 
 /**
  * Start up the resources subsystem.
  */
-extern struct Ratr0ResourceSystem *ratr0_resources_startup(void);
+extern struct Ratr0ResourceSystem *ratr0_resources_startup(Ratr0Engine *);
 
 #endif /* __RATR0_RESOURCES_H__ */
--- src/resources.c
+++ src/resources.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ratr0/debug_utils.h>
+#include <ratr0/memory.h>
 #include <ratr0/resources.h>
 
 #ifdef AMIGA
@@ -9,11 +10,18 @@
 #endif
 
 void ratr0_resources_shutdown(void);
-static struct Ratr0ResourceSystem resource_system;
+UINT32 ratr0_read_tilesheet(const char *filename, struct Ratr0TileSheet *sheet);
+void ratr0_free_tilesheet_data(struct Ratr0TileSheet *sheet);
 
-struct Ratr0ResourceSystem *ratr0_resources_startup(void)
+static struct Ratr0ResourceSystem resource_system;
+static Ratr0Engine *engine;
+
+struct Ratr0ResourceSystem *ratr0_resources_startup(Ratr0Engine *eng)
 {
+    engine = eng;
     resource_system.shutdown = &ratr0_resources_shutdown;
+    resource_system.read_tilesheet = &ratr0_read_tilesheet;
+    resource_system.free_tilesheet_data = &ratr0_free_tilesheet_data;
     PRINT_DEBUG("Startup finished.");
     return &resource_system;
 }
@@ -21,4 +29,35 @@ struct Ratr0ResourceSystem *ratr0_resources_startup(void)
 void ratr0_resources_shutdown(void)
 {
     PRINT_DEBUG("Shutdown finished.");
+}
+
+UINT32 ratr0_read_tilesheet(const char *filename, struct Ratr0TileSheet *sheet)
+{
+    int elems_read;
+    UINT32 retval = 0;
+
+    FILE *fp = fopen(filename, "rb");
+
+    if (fp) {
+        int num_img_bytes;
+        elems_read = fread(&sheet->header, sizeof(struct Ratr0TileSheetHeader), 1, fp);
+        elems_read = fread(&sheet->palette, sizeof(UWORD), sheet->header.palette_size, fp);
+        Ratr0MemHandle handle = engine->memory_system->allocate_block(RATR0_MEM_CHIP, sheet->header.imgdata_size);
+        sheet->h_imgdata = handle;
+        UINT8 *imgdata = engine->memory_system->block_address(handle);
+        elems_read = fread(imgdata, sizeof(unsigned char), sheet->header.imgdata_size, fp);
+        fclose(fp);
+        return 1;
+    } else {
+        printf("ratr0_read_tilesheet() error: file '%s' not found\n", filename);
+        return 0;
+    }
+}
+
+/**
+ * Frees the memory that was allocated for the specified RATR0 tile sheet.
+ */
+void ratr0_free_tilesheet_data(struct Ratr0TileSheet *sheet)
+{
+    if (sheet && sheet->h_imgdata) engine->memory_system->free_block(sheet->h_imgdata);
 }
