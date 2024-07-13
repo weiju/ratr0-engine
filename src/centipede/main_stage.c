6d0bd473008134e7aac2248bcdd7ffc69d061e70
weiju <weiju.wu@gmail.com>
2024-07-13 08:37:37 -0700

    added default copper list file
      * ratr0-makecoplist is now part of ratr0-utils
      * generate default copper list in Makefile
      * use default copper list in display.c
      * ratr0_set_sprite() in display.c fixed, was buggy index
      * also added diagnostic function to dump copper list to file

--- src/centipede/main_stage.c
+++ src/centipede/main_stage.c
@@ -10,10 +10,9 @@ static Ratr0Engine *engine;
 extern RATR0_ACTION_ID action_fire, action_move_left, action_move_right;
 
 // Resources
-#define GRID_PATH ("sinvaders/assets/basegrid_288x256x2.ts")
 #define CENTI_HEAD_LR_PATH ("centipede/assets/c_head_l2r.ts")
-struct Ratr0TileSheet grid_sheet, centi_head_lr_sheet;
-struct Ratr0SpriteSheet sprite_sheet;
+struct Ratr0TileSheet grid_sheet;
+struct Ratr0TileSheet centi_head_lr_sheet;
 UINT8 centi_head_lr_frames[] = {0, 1, 2, 3, 4};
 
 void main_scene_update(struct Ratr0Scene *this_scene, UINT8 frames_elapsed)
@@ -38,9 +37,6 @@ struct Ratr0Scene *setup_main_scene(Ratr0Engine *eng)
     struct Ratr0Scene *main_scene = node_factory->create_scene();
     main_scene->update = main_scene_update;
 
-    engine->resource_system->read_tilesheet(GRID_PATH, &grid_sheet);
-    ratr0_display_set_palette(grid_sheet.palette, 4, 0);
-
     // can we do this in one step ?
     engine->resource_system->read_tilesheet(CENTI_HEAD_LR_PATH, &centi_head_lr_sheet);
     struct Ratr0AnimatedHWSprite *anim_sprite = (struct Ratr0AnimatedHWSprite *)
@@ -50,9 +46,5 @@ struct Ratr0Scene *setup_main_scene(Ratr0Engine *eng)
 
     anim_sprite->base_obj.bounds.x = 0;
     anim_sprite->base_obj.bounds.y = 10;
-
-    struct Ratr0Surface *back_buffer, *front_buffer;
-    front_buffer = ratr0_get_front_buffer();
-    back_buffer = ratr0_get_back_buffer();
     return main_scene;
 }
